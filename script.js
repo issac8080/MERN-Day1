0fe929cf038429f6602d49b974d46160a0758cc5
issac8080 <98963875+issac8080@users.noreply.github.com>
2023-05-02 15:53:48 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,141 @@
+//console.log("Hello World")
+//var let const
+/*var a=10;
+console.log(a);
+var a=20;
+console.log(a);
+let b=23
+
+let a =10;
+let b= 20;
+let c=a-b;
+let d=a*b;
+let e=a/b;
+let f=a%b;
+console.log(a+b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+
+let a=10;
+let b="10";
+if(a==b){
+    console.log(true)
+}else{
+    console.log(false)
+}
+
+
+let f = "issac";
+let g=" sunny";
+let h=f+g;
+//console.log(h)
+console.log(`${f} and ${g}`);
+
+
+let car={
+    model:"celerio",
+    color:"white",
+    price: 100,
+}
+console.log(car);
+console.log(car.color);
+console.log(car.model);
+console.log(`my car is ${car.color} ${car.model}`)
+
+
+
+let person={
+    name: "Issac",
+    college: "CCE",
+    class: "S4",
+    address:{
+        housename:"ijk",
+        street:"tcr"
+}}
+console.log(person.name);
+console.log(person.college);
+console.log(person.class);
+console.log(person.address.housename);
+console.log(`${person.name} ${person.address.housename}`);
+
+
+//array
+
+let fruits=["apple","mango","orange",10]
+fruits[3]="grapes";
+fruits.push("pineapple")
+fruits.unshift("drangon")
+fruits.pop();
+fruits.shift();
+console.log(fruits)
+
+let todo=[
+    {
+        id:1,
+
+    }
+]
+
+
+let students =[
+    {name: "Issac",age:20},{name:"Sunny",age:20}]
+
+console.log(students[1].name)
+
+
+Function  
+
+function addTwoNumbers(a,b){
+    console.log(a+b);
+
+}
+addTwoNumbers(10,20);
+
+
+function subTwoNumbers(a,b){
+
+console.log(a-b);}
+subTwoNumbers(10,23);
+
+add=(a,b)=>{console.log(a+b);};
+subTwoNumbers(10,30);
+add(45,63);
+add=(a,b)=>
+{let d= a+b;
+    if(d>10)
+    {console.log("is greater");
+}
+else
+{
+    console.log("not greater");
+}
+}
+add(90,6)*/
+
+let numbers=[1,2,3,4,5,6,7,8,9,10];
+console.log(numbers);
+/*print=(a)=>
+{
+    for(i=0;i<=9;i++)
+    {console.log(a[i])}
+}
+print(numbers);
+print=(a)=>
+{i=0;
+while(i<10)
+{
+    console.log(a[i]);
+    i++;
+}}
+print(numbers);
+*/
+print=(a)=>
+{i=0;
+do{
+    console.log(a[i]);
+    i++;
+
+}while(i<10)}
+print(numbers);
